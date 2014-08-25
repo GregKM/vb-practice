46154b6dfb089054cc51fa7185d9fa022a642f24
GregKM <gregkmathews@gmail.com>
2014-08-25 01:01:52 -0400

    Created Variables project.

---
+++ Variables/Variables/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Dim x As Integer
+        'Dim y As Integer
+        'x = 7
+        'y = x + 3
+        'Console.WriteLine(y)
+
+        'Dim myFirstName As String
+        'myFirstName = "Bob"
+
+        'Dim myFirstName As String = "Bob"
+
+        'Dim myFirstName = "Bob"
+        'Console.WriteLine(myFirstName)
+
+        Dim x As Integer = 7
+        'Dim y As String = "Bob"
+        Dim y As Integer = 5
+        Dim myFirstTry As String = CStr(x) & y
+        Console.WriteLine(myFirstTry)
+
+        'Dim mySecondTry As Integer = x + y
+        'Console.WriteLine(mySecondTry)
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
