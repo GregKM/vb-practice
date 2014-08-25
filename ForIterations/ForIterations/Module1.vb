43c0cbb9c8f6b29528a31e4c5c197c469047b909
GregKM <gregkmathews@gmail.com>
2014-08-25 01:11:23 -0400

    Created project ForIterations.

---
+++ ForIterations/ForIterations/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'For index = 1 To 10
+        '    If index = 7 Then
+        '        Console.WriteLine("Found seven!")
+        '        Exit For
+        '    End If
+        '    Console.WriteLine(index)
+        'Next
+
+        For x = 10 To 5 Step -1
+            Console.WriteLine(x)
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
